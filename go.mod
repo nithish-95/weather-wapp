19c86f2e085505bda866761ad37f25a996a21471
Nithish S <nithish952001@gmail.com>
2024-05-28 13:05:12 -0700

    weatherReport handler func

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module nithish-95/weather-wapp.git
 
 go 1.21.7
+
+require github.com/go-chi/chi/v5 v5.0.12
--- weather.go
+++ weather.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func weatherReporter(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Weather web app demo"))
+}
 
 func main() {
-	fmt.Println("Weather web app demo")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", weatherReporter)
+
+	http.ListenAndServe(":3000", r)
+
 }
