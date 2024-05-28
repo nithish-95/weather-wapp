63bfd74be082fd7f9d2aee0189713bced7af3355
Nithish S <nithish952001@gmail.com>
2024-05-28 13:38:24 -0700

    add api key and error handling

--- weather.go
+++ weather.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+const apiKey = "b3e15c4737290fe97fed1c359bd47d12"
+
 func weatherReporter(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Weather web app demo"))
+	zipcode := chi.URLParam(r, "zipcode")
+
+	if zipcode == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	resp := fmt.Sprintf("Weather at the %v is -----", zipcode)
+	w.Write([]byte(resp))
 }
 
 func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", weatherReporter)
+	r.Get("/{zipcode}", weatherReporter)
 
-	http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		fmt.Print(err)
+	}
 
 }
