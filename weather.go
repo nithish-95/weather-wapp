8e5b89e0aecab21b4a5fbaf9e048634c7ceef2bb
Nithish S <nithish952001@gmail.com>
2024-05-31 18:20:14 -0700

    edited the WeatherResponse struct

--- weather.go
+++ weather.go
@@ -21,9 +21,26 @@ type WeatherResponse struct {
 		Description string `json:"description"`
 		Icon        string `json:"icon"`
 	} `json:"weather"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
 	Main struct {
-		Temp float64 `json:"temp"`
+		Temp     float64 `json:"temp"`
+		Pressure int     `json:"pressure"`
+		Humidity int     `json:"humidity"`
+		TempMin  float64 `json:"temp_min"`
+		TempMax  float64 `json:"temp_max"`
 	} `json:"main"`
+	Sys struct {
+		Type    int     `json:"type"`
+		ID      int     `json:"id"`
+		Message float64 `json:"message"`
+		Country string  `json:"country"`
+		Sunrise int64   `json:"sunrise"`
+		Sunset  int64   `json:"sunset"`
+	} `json:"sys"`
 }
 
 type IpResponse struct {
